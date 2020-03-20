bfa327c93ea05e9d1554b05ba19f96d89757e8dd
Brett Zamir <brettz9@yahoo.com>
2020-03-20 17:36:14 +0800

    - Replace deprecated `new Buffer` with `Buffer.from`

--- test/test.js
+++ test/test.js
@@ -7,35 +7,47 @@ describe('CLI', function () {
     expect(stderr).to.equal('');
     expect(stdout).to.contain('Usage: pop [options]');
   });
-  it('Errs upon invalid alias', async function () {
+  it('Runs QUIT', async function () {
     const {stdout, stderr} = await spawnAsync('./bin/pop.js', [
-      '-x'
-    ]);
-    expect(stderr).to.equal('Invalid alias x\n');
-    expect(stdout).to.equal('');
-  });
-  it('Errs upon invalid argument', async function () {
-    const {stdout, stderr} = await spawnAsync('./bin/pop.js', [
-      'xyz'
-    ]);
-    expect(stderr).to.equal('Invalid argument xyz\n');
-    expect(stdout).to.equal('');
-  });
-  it('Errs upon missing method', async function () {
-    let {stdout, stderr} = await spawnAsync('./bin/pop.js', [
-      '--config',
-      'pop.config.json'
-    ]);
-    expect(stderr).to.equal('method is required!\n');
-    expect(stdout).to.contain('Usage: pop [options]');
-
-    ({stdout, stderr} = await spawnAsync('./bin/pop.js', [
       '--config',
       'pop.config.json',
-      '-u',
-      'brett@example.name'
-    ]));
-    expect(stderr).to.equal('method is required!\n');
-    expect(stdout).to.contain('Usage: pop [options]');
+      '--method',
+      'QUIT'
+    ]);
+    expect(stderr).to.equal('');
+    expect(stdout).to.equal('\'Bye\'\n');
+  });
+  describe('Errors', function () {
+    it('Errs upon invalid alias', async function () {
+      const {stdout, stderr} = await spawnAsync('./bin/pop.js', [
+        '-x'
+      ]);
+      expect(stderr).to.equal('Invalid alias x\n');
+      expect(stdout).to.equal('');
+    });
+    it('Errs upon invalid argument', async function () {
+      const {stdout, stderr} = await spawnAsync('./bin/pop.js', [
+        'xyz'
+      ]);
+      expect(stderr).to.equal('Invalid argument xyz\n');
+      expect(stdout).to.equal('');
+    });
+    it('Errs upon missing method', async function () {
+      let {stdout, stderr} = await spawnAsync('./bin/pop.js', [
+        '--config',
+        'pop.config.json'
+      ]);
+      expect(stderr).to.equal('method is required!\n');
+      expect(stdout).to.contain('Usage: pop [options]');
+
+      ({stdout, stderr} = await spawnAsync('./bin/pop.js', [
+        '--config',
+        'pop.config.json',
+        '-u',
+        'brett@example.name'
+      ]));
+      expect(stderr).to.equal('method is required!\n');
+      expect(stdout).to.contain('Usage: pop [options]');
+    });
   });
 });
